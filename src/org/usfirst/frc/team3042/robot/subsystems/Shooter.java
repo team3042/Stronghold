1bbe6490c792289e587cefa63007c6c987ab092f
rdpalmer <rdpalmer@comcast.net>
2016-02-16 22:13:55 -0600

    Local Commit

--- src/org/usfirst/frc/team3042/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team3042/robot/subsystems/Shooter.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team3042.robot.subsystems;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team3042.robot.RobotMap;
 import org.usfirst.frc.team3042.robot.commands.Shooter_Stop;
