d778328bce08f1554bbf78cb8ea117e28cd30e1a
FRCTeam3042 <team3042@gmail.com>
2016-04-03 13:56:42 -0500

    Beginning of Chanhassen Practice

--- src/org/usfirst/frc/team3042/robot/subsystems/CameraAPI.java
+++ src/org/usfirst/frc/team3042/robot/subsystems/CameraAPI.java
@@ -54,9 +54,14 @@ public class CameraAPI extends Subsystem {
 	public static NIVision.Range TARGET_VAL_RANGE = new NIVision.Range(109, 255);	//Range for green light
 	*/
 	/*Daytime Commons 8*/
-	public static NIVision.Range TARGET_HUE_RANGE = new NIVision.Range(92, 137);	//Range for green light
-	public static NIVision.Range TARGET_SAT_RANGE = new NIVision.Range(140, 255);	//Range for green light
-	public static NIVision.Range TARGET_VAL_RANGE = new NIVision.Range(87, 255);	//Range for green light
+	//public static NIVision.Range TARGET_HUE_RANGE = new NIVision.Range(92, 137);	//Range for green light
+	//public static NIVision.Range TARGET_SAT_RANGE = new NIVision.Range(140, 255);	//Range for green light
+	//public static NIVision.Range TARGET_VAL_RANGE = new NIVision.Range(87, 255);	//Range for green light
+	
+	//Chanhassen
+	public static NIVision.Range TARGET_HUE_RANGE = new NIVision.Range(85, 255);	//Range for green light
+	public static NIVision.Range TARGET_SAT_RANGE = new NIVision.Range(0, 255);	//Range for green light
+	public static NIVision.Range TARGET_VAL_RANGE = new NIVision.Range(161, 255);	//Range for green light
 
 	//Lights Off - B123
 //	public static NIVision.Range TARGET_HUE_RANGE = new NIVision.Range(75, 130);
@@ -97,7 +102,7 @@ public class CameraAPI extends Subsystem {
 	//Fenrir offset at 320x240 is -16.5. 
 	//Scaled up to 480x360 I expect it to be -25
 	//If hitting right make less negative, to the left change to more negative.
-	double OFFSET_ZERO = (RobotMap.isSkoll) ? -45 : -63.5;
+	double OFFSET_ZERO = (RobotMap.isSkoll) ? -65 : -63.5;
 	
 	public CameraAPI(){
 		camera.writeCompression(30);
